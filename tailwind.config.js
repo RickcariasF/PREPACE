9cb155fe7169e797374848752c1f588741090548
Ricardo <ricardocariasf@hotmail.com>
2024-07-22 21:08:22 -0600

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './**/*.hbs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
