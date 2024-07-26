66bb6867f1150938fbe4eb8f3a56694c29a19b4e
Ricardo <ricardocariasf@hotmail.com>
2024-07-26 16:18:53 -0600

    Cuarto intento de corregir burgerjs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
     document.addEventListener('DOMContentLoaded', function() {
-        const menuButton = document.querySelector('button[type="button"]');
+        const menuButton = document.getElementById('menuButton');
         const mobileMenu = document.querySelector('div[role="dialog"]');
         const closeButton = mobileMenu.querySelector('button[type="button"]');
 
